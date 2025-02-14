db53f3a8abfae985daf31c074d742b922243e33d
breno lira <brenolirarocha5@gmail.com>
2025-02-14 10:53:21 -0300

    novo commit

--- Prova/prova.js
+++ Prova/prova.js
@@ -1,14 +1,43 @@
-const response = fetch('https://jsonplaceholder.typicode.com5/posts')
+// const response = fetch('https://jsonplaceholder.typicode.com/posts')
 
-.then (function (responseData) {
-    return responseData.json();
-})
+// .then (function (responseData) {
+//     return responseData.json();
+// })
 
-.then (function (josonData) {
-    console.log(josonData)
-})
+// .then (function (josonData) {
+//     console.log(josonData)
+// })
 
-.catch (function (erro) {
-    console.log('Erro ao buscar a API ---> ' + response.status)
-})
+// .catch (function (erro) {
+//     console.log('Erro ao buscar a API ---> ' + response.status)
+// })
 
+(async function(){
+    try {
+        const headers = {
+            'Content-Type': 'application/json',
+            'Teste-Header': 'Teste valido',
+            'Authorization': 'Bearer ufhdsiuhfdufhsiufdfy',
+        };
+
+        const init = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify ({
+                title: 'Testede criação'
+            })
+        }
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts',
+            init
+        )
+
+        const jsonData = await response.json();
+
+        console.log(jsonData)
+
+        } catch (erro) {
+            console.log('Erro ao buscar a API')
+        }
+
+})();
